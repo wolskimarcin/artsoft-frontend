ccc1de903c199f582d393e556bbc0ee5a812189d
wolskimarcin <wolski.marcin.it@gmail.com>
2024-02-03 14:55:58 +0100

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,12 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {RegistrationComponent} from './registration/registration.component';
 import {JwtInterceptor} from "./interceptors/jwt-interceptor.service";
 import { HomeComponent } from './home/home.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
 
 @NgModule({
   declarations: [
@@ -26,14 +32,20 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormField,
+    MatInput,
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: JwtInterceptor,
       multi: true
-    }],
+    },
+    provideAnimationsAsync()],
   bootstrap: [AppComponent]
 })
 export class AppModule {
