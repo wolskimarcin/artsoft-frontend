25ed07c6599e32417361ac4b0827752efe7dfb26
wolskimarcin <wolski.marcin.it@gmail.com>
2024-02-03 22:43:26 +0100

    Handle search bar, products filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,10 +8,15 @@ import {AuthService} from "./auth.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  searchTerm: string = '';
   title = 'artsoft-frontend';
   itemCount: number = 0; // TODO: Update based on actual cart items
   totalPrice: number = 0.00; // TODO: Update based on actual cart items total price
 
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private router: Router) {
+  }
+
+  executeSearch() {
+    this.router.navigate(['/products'], { queryParams: { search: this.searchTerm } });
   }
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -3,6 +3,7 @@ import {ProductControllerService} from "../api/services/product-controller.servi
 import {Product} from "../api/models/product";
 import {GetProducts$Params} from "../api/fn/product-controller/get-products";
 import {PageProduct} from "../api/models/page-product";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-products',
@@ -16,16 +17,26 @@ export class ProductsComponent implements OnInit {
   totalElements: number = 0;
   totalPages: number = 0;
 
-  constructor(private productService: ProductControllerService) {}
+  constructor(
+    private productService: ProductControllerService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.fetchProducts(this.currentPage, this.pageSize);
+    this.route.queryParams.subscribe(params => {
+      const search = params['search'] || '';
+      const page = parseInt(params['page'] || '0', 10);
+      const size = parseInt(params['size'] || '10', 10);
+
+      this.fetchProducts(page, size, search);
+    });
   }
 
-  fetchProducts(page: number, size: number): void {
+  fetchProducts(page: number, size: number, search: string = ''): void {
     const params: GetProducts$Params = {
       page: page,
-      size: size
+      size: size,
+      searchTerm: search
     };
 
     this.productService.getProducts(params).subscribe({
@@ -35,11 +46,8 @@ export class ProductsComponent implements OnInit {
         this.pageSize = pageData.size || 10;
         this.totalElements = pageData.totalElements || 0;
         this.totalPages = pageData.totalPages || 0;
-        console.log('Received product data:', pageData);
       },
-      error: (error) => {
-        console.error('Error fetching products:', error);
-      }
+      error: (error) => console.error('Error fetching products:', error)
     });
   }
 
